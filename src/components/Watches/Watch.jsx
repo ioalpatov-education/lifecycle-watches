f3a359499b71026ae7dc3652bc16eefa4a1a57ce
IlyaAlpatov <alpatov30@gmail.com>
2023-04-12 21:14:19 +0300

    добавлено движение стрелок

--- src/components/Watches/Watch.jsx
+++ src/components/Watches/Watch.jsx
@@ -1,15 +1,29 @@
 import { Close } from "@mui/icons-material";
 import PropTypes from "prop-types";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Watch = ({ watch, onDeleteWatch }) => {
   const { id, title, startDate } = watch;
 
+  const [time, setTime] = useState(null);
+
+  let hourStyles;
+  let minutesStyles;
+  let secondsStyles;
   useEffect(() => {
-    let newDate = startDate;
+    let currentDate = startDate;
     const intervalId = setInterval(() => {
-      newDate.setSeconds(newDate.getSeconds() + 1);
-      console.log(new Date(newDate));
+      const hour = currentDate.getHours();
+      const minutes = currentDate.getMinutes();
+      const seconds = currentDate.getSeconds();
+
+      setTime({
+        hour,
+        minutes,
+        seconds,
+      });
+
+      currentDate.setSeconds(currentDate.getSeconds() + 1);
     }, 1000);
 
     return () => {
@@ -17,23 +31,42 @@ const Watch = ({ watch, onDeleteWatch }) => {
     };
   });
 
+  if (!!time) {
+    hourStyles = {
+      transform: `rotate(${time.hour * 30}deg)`,
+    };
+
+    minutesStyles = {
+      transform: `rotate(${time.minutes * 6}deg)`,
+    };
+
+    secondsStyles = {
+      transform: `rotate(${time.seconds * 6}deg)`,
+    };
+  }
+
   const deleteWatch = () => {
     onDeleteWatch(id);
   };
 
-  return (
+  const watchMarkup = !!time ? (
     <div className="watch-wrapper">
       <p>{title}</p>
       <div className="watch">
-        <div className="watch__hand second-hand"></div>
-        <div className="watch__hand minute-hand arrow"></div>
-        <div className="watch__hand hour-hand arrow"></div>
+        <div className="watch__hand second-hand" style={secondsStyles}></div>
+        <div
+          className="watch__hand minute-hand arrow"
+          style={minutesStyles}
+        ></div>
+        <div className="watch__hand hour-hand arrow" style={hourStyles}></div>
       </div>
       <button className="watch__delete-btn" onClick={deleteWatch}>
         <Close className="delete-btn__icon" />
       </button>
     </div>
-  );
+  ) : null;
+
+  return <>{watchMarkup}</>;
 };
 
 Watch.propTypes = {
